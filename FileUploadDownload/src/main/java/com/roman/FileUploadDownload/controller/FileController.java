1733e18f637481973793e7fbb8e194ab89870d98
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-02-24 22:00:57 +0600

    Update controller

--- FileUploadDownload/src/main/java/com/roman/FileUploadDownload/controller/FileController.java
+++ FileUploadDownload/src/main/java/com/roman/FileUploadDownload/controller/FileController.java
@@ -4,21 +4,23 @@ package com.roman.FileUploadDownload.controller;
 import com.roman.FileUploadDownload.model.FileEntity;
 import com.roman.FileUploadDownload.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/files")
@@ -46,5 +48,26 @@ public class FileController {
         }
     }
 
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> handleFIleDownload(@PathVariable Long fileId) {
 
+        Optional<FileEntity> optionalFile = fileService.getFileById(fileId);
+        if (optionalFile.isPresent()) {
+            FileEntity file = optionalFile.get();
+            Path filePath = Paths.get(file.getFilePath());
+
+            Resource resource;
+            try {
+                resource = new UrlResource(filePath.toUri());
+            } catch (MalformedURLException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getFileName())
+                    .body(resource);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
