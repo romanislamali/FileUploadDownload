a1f5a327508bd47d16754c936dac091d1960fdd4
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-02-24 20:17:43 +0600

    Entity create

---
+++ FileUploadDownload/src/main/java/com/roman/FileUploadDownload/model/FileEntity.java
@@ -0,0 +1,20 @@
+package com.roman.FileUploadDownload.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name ="files")
+@Data
+public class FileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String filePath;
+}
