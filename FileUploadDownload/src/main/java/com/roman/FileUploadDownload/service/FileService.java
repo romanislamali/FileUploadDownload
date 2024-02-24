aa860dd42e3cdec9530a21328bd9d0b8d3525564
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-02-24 23:52:25 +0600

    Service Optimization

--- FileUploadDownload/src/main/java/com/roman/FileUploadDownload/service/FileService.java
+++ FileUploadDownload/src/main/java/com/roman/FileUploadDownload/service/FileService.java
@@ -3,8 +3,22 @@ package com.roman.FileUploadDownload.service;
 import com.roman.FileUploadDownload.model.FileEntity;
 import com.roman.FileUploadDownload.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -13,11 +27,41 @@ public class FileService {
     @Autowired
     private FileRepository fileRepository;
 
-    public FileEntity saveFile(FileEntity file){
-        return fileRepository.save(file);
+    public ResponseEntity<String> saveFile(MultipartFile file){
+        String uploadDirectory = "src/main/resources/uploads/";
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        Path uploadPath = Paths.get(uploadDirectory);
+        try{
+            Files.copy(file.getInputStream(), uploadPath.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+            FileEntity fileEntity = new FileEntity();
+                fileEntity.setFileName(fileName);
+                fileEntity.setFilePath(uploadPath.resolve(fileName).toString());
+                fileRepository.save(fileEntity);
+                return ResponseEntity.ok("File Uploaded Successful!!");
+        }catch (IOException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to Upload file!!");
+        }
     }
 
-    public Optional<FileEntity> getFileById(Long fileId){
-        return fileRepository.findById(fileId);
+
+    public ResponseEntity<Resource> getFileById(Long fileId){
+        Optional<FileEntity> optionalFile = fileRepository.findById(fileId);
+        if (optionalFile.isPresent()) {
+            FileEntity file = optionalFile.get();
+            Path filePath = Paths.get(file.getFilePath());
+
+            Resource resource;
+            try {
+                resource = new UrlResource(filePath.toUri());
+            } catch (MalformedURLException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getFileName())
+                    .body(resource);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
