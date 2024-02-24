ac6b0a27ae913f5bb40df10b9f601c1dabc352c4
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-02-24 21:31:32 +0600

    Create Service

---
+++ FileUploadDownload/src/main/java/com/roman/FileUploadDownload/service/FileService.java
@@ -0,0 +1,21 @@
+package com.roman.FileUploadDownload.service;
+
+import com.roman.FileUploadDownload.model.FileEntity;
+import com.roman.FileUploadDownload.repository.FileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FileService {
+
+    private FileRepository fileRepository;
+
+    public FileEntity saveFile(FileEntity file){
+        return fileRepository.save(file);
+    }
+
+    public Optional<FileEntity> getFileById(Long fileId){
+        return fileRepository.findById(fileId);
+    }
+}
